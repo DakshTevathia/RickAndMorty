84add6ce17dc58d1cdf786c0eece0ebc746e0f13
Daksh-Springr <daksh.tevathia@springr.in>
2022-12-19 13:15:24 +0530

    Made changes in the charcter card details UI

--- src/Components/Cards/CharacterCard.js
+++ src/Components/Cards/CharacterCard.js
@@ -23,26 +23,42 @@ const CharacterCard = () => {
     }, [url]);
 
     return (
-        <div className='container' style={{
+        <div style={{
             display: 'flex',
             justifyContent: 'center',
-            marginTop: '20px',
-            marginBottom: '50px',
+            height: '80vh',
             alignItems: 'center'
         }}>
-            <div className='row'>
-                <div className='col'>
-                    <div className='row'>
-                        <div className='col'>
-                            <img style={{
-                                border: 'solid #1bff00e0 2px',
-                                borderRadius: '5px'
-                            }} src={characterinfo.image} alt="CharacterImage" />
+            <div style={{
+                display: 'flex',
+                alignItems: 'stretch',
+                columnGap: '2rem',
+                justifyContent: 'center'
+            }}>
+                <div>
+                    <img style={{
+                        border: 'solid #1bff00e0 2px',
+                        borderRadius: '5px'
+                    }} src={characterinfo.image} alt="CharacterImage" />
+                </div>
+                <div style={{ marginTop: '4px', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+                    <div className='row' style={{ width: '304px' }}>
+                        <div className='col' style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            border: 'solid 2px #1bff00e0',
+                            marginTop: '0.3rem',
+                            borderRadius: '5px',
+                            fontFamily: "Lucida Console",
+                            color: '#0d6efd',
+                        }}>
+                            Name: {characterinfo?.name}
                         </div>
                     </div>
-                    <div style={{ marginTop: '4px', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-                        <div className='row' style={{ width: '304px' }}>
-                            <div className='col' style={{
+                    <div className='row' style={{ width: '304px' }}>
+                        <div className='col'
+                            style={{
                                 display: 'flex',
                                 alignItems: 'center',
                                 justifyContent: 'center',
@@ -50,85 +66,69 @@ const CharacterCard = () => {
                                 marginTop: '0.3rem',
                                 borderRadius: '5px',
                                 fontFamily: "Lucida Console",
-                                color: '#0d6efd',
+                                color: '#0d6efd'
                             }}>
-                                Name: {characterinfo?.name}
-                            </div>
+                            Location: {characterinfo?.location?.name}
                         </div>
-                        <div className='row' style={{ width: '304px' }}>
-                            <div className='col'
-                                style={{
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    border: 'solid 2px #1bff00e0',
-                                    marginTop: '0.3rem',
-                                    borderRadius: '5px',
-                                    fontFamily: "Lucida Console",
-                                    color: '#0d6efd'
-                                }}>
-                                Location: {characterinfo?.location?.name}
-                            </div>
+                    </div>
+                    <div className='row' style={{ width: '304px' }}>
+                        <div className='col'
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                border: 'solid 2px #1bff00e0',
+                                marginTop: '0.3rem',
+                                borderRadius: '5px',
+                                fontFamily: "Lucida Console",
+                                color: '#0d6efd'
+                            }}>
+                            Species: {characterinfo.species}
                         </div>
-                        <div className='row' style={{ width: '304px' }}>
-                            <div className='col'
-                                style={{
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    border: 'solid 2px #1bff00e0',
-                                    marginTop: '0.3rem',
-                                    borderRadius: '5px',
-                                    fontFamily: "Lucida Console",
-                                    color: '#0d6efd'
-                                }}>
-                                Species: {characterinfo.species}
-                            </div>
+                    </div>
+                    <div className='row' style={{ width: '304px' }}>
+                        <div className='col'
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                border: 'solid 2px #1bff00e0',
+                                marginTop: '0.3rem',
+                                borderRadius: '5px',
+                                fontFamily: "Lucida Console",
+                                color: '#0d6efd'
+                            }}>
+                            Status: {characterinfo.status}
                         </div>
-                        <div className='row' style={{ width: '304px' }}>
-                            <div className='col'
-                                style={{
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    border: 'solid 2px #1bff00e0',
-                                    marginTop: '0.3rem',
-                                    borderRadius: '5px',
-                                    fontFamily: "Lucida Console",
-                                    color: '#0d6efd'
-                                }}>
-                                Status: {characterinfo.status}
-                            </div>
+                    </div>
+                    <div className='row' style={{ width: '304px' }}>
+                        <div className='col'
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                border: 'solid 2px #1bff00e0',
+                                marginTop: '0.3rem',
+                                borderRadius: '5px',
+                                fontFamily: "Lucida Console",
+                                color: '#0d6efd'
+                            }}>
+                            Gender: {characterinfo.gender}
                         </div>
-                        <div className='row' style={{ width: '304px' }}>
-                            <div className='col'
-                                style={{
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    border: 'solid 2px #1bff00e0',
-                                    marginTop: '0.3rem',
-                                    borderRadius: '5px',
-                                    fontFamily: "Lucida Console",
-                                    color: '#0d6efd'
-                                }}>
-                                Gender: {characterinfo.gender}
-                            </div>
-                        </div>
-                        <div className='row' style={{ width: '304px' }}>
-                            <div className='col'
-                                style={{
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    border: 'solid 2px #1bff00e0',
-                                    marginTop: '0.3rem',
-                                    borderRadius: '5px',
-                                    fontFamily: "Lucida Console",
-                                    color: '#0d6efd'
-                                }}>
-                                {characterinfo.type === '' ? 'Type: Regular' : 'Type: ' + characterinfo.type}
-                            </div>
+                    </div>
+                    <div className='row' style={{ width: '304px' }}>
+                        <div className='col'
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                border: 'solid 2px #1bff00e0',
+                                marginTop: '0.3rem',
+                                borderRadius: '5px',
+                                fontFamily: "Lucida Console",
+                                color: '#0d6efd'
+                            }}>
+                            {characterinfo.type === '' ? 'Type: Regular' : 'Type: ' + characterinfo.type}
                         </div>
                     </div>
                 </div>
